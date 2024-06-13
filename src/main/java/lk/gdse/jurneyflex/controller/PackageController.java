affaaff40dd38556be82943021d68cdbbe3a7577
AmilSrinath <amilsrinath5@gmail.com>
2024-06-13 23:15:15 +0530

    Create PackageContoller

--- src/main/java/lk/gdse/jurneyflex/controller/PackageController.java
+++ src/main/java/lk/gdse/jurneyflex/controller/PackageController.java
@@ -1,5 +1,21 @@
 package lk.gdse.jurneyflex.controller;
 
-public class PackageController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+public class PackageController {
+    @GetMapping("/healthCheck")
+    public String healthCheck(){
+        return "Healthy";
+    }
+
+    @PostMapping("/addStaticPackage")
+    public String addStaticPackage(){
+        return "Add Static Package";
+    }
+
+    @PostMapping("/addCustomPackage")
+    public String addCustomPackage(){
+        return "Add Custom Package";
+    }
 }
