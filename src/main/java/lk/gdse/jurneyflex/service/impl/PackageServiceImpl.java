59c9327aa688f4895573d293b2fa005c3f5aec16
Sahan2000 <udanasahan1@gmail.com>
2024-06-14 11:24:56 +0530

    add packageService implementation

--- src/main/java/lk/gdse/jurneyflex/service/impl/PackageServiceImpl.java
+++ src/main/java/lk/gdse/jurneyflex/service/impl/PackageServiceImpl.java
@@ -1,15 +1,35 @@
 package lk.gdse.jurneyflex.service.impl;
 
 import jakarta.transaction.Transactional;
+<<<<<<< HEAD
+=======
+import lk.gdse.jurneyflex.conversion.ConversionData;
+>>>>>>> e63abf6 (add packageService implementation)
 import lk.gdse.jurneyflex.dto.PackageDTO;
+import lk.gdse.jurneyflex.entity.Package;
+import lk.gdse.jurneyflex.repository.PackageServiceDao;
 import lk.gdse.jurneyflex.service.PackageService;
+<<<<<<< HEAD
 import org.springframework.stereotype.Service;
 
 @Transactional
 @Service
+=======
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+>>>>>>> e63abf6 (add packageService implementation)
 public class PackageServiceImpl implements PackageService {
+    @Autowired
+    private PackageServiceDao packageServiceDao;
+    @Autowired
+    private ConversionData convert;
     @Override
     public void addPackage(PackageDTO packageDTO) {
-
+        convert.packagetoPackageDto(packageServiceDao.save(convert.packageDtoToPackage(packageDTO)));
     }
 }
